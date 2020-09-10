f8f50ef56e12b746b1080e5b48b8828c9afcb80b
Nikitos-div <motiki839@gmail.com>
2020-09-10 20:20:08 +0300

    init reader version 1.0.0

---
+++ index.js
@@ -0,0 +1,37 @@
+
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const path = require('path')
+require('dotenv').config()
+const cors = require('cors')
+
+
+app.use(cors())
+app.use('/', express.static(__dirname + '/dist'));
+
+
+
+
+app.get('/files', (req, res) => {
+    const readFile = () => new Promise((resolve, reject) => {
+        fs.readFile('./files/New_Horizons.pdf', (err, data) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(data)
+            }
+        })
+    })
+    readFile()
+        .then(data => res.send(data))
+        .catch(err => res.send(err))
+})
+
+let PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
+})
+
+
